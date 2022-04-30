d4e095fa862d0dcf3f165f7d71401895e2e035a2
MdMehedi-Hasan <mh19115@gmail.com>
2022-04-30 06:38:23 +0600

    connected with mongo and send basic data

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
-
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// MongoDB connection starts here (version: 4.0 or later)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.avcst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// const collection = client.db("test").collection("devices");
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("Eastwood").collection("products");
+    // This is the collection of all products. All the data from database is gathering in this route.
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray(query);
+      res.send(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// MongoDB connection ends
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
