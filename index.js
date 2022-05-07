1a017d5f2ed179811e9929dad819c62e9834d1c0
MdMehedi-Hasan <mh19115@gmail.com>
2022-05-07 23:50:36 +0600

    manage and myitem route done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -8,10 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 // MongoDB connection starts here (version: 4.0 or later)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.avcst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -19,8 +15,6 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-// const collection = client.db("test").collection("devices");
-
 async function run() {
   try {
     await client.connect();
@@ -32,6 +26,42 @@ async function run() {
       const products = await cursor.toArray(query);
       res.send(products);
     });
+    // Here we are finding a single product with id (This api is collaborating with homepage inventory section)
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+    // In below code we are trying to add a new product to the server
+    app.post("/product", async (req, res) => {
+      const singleProduct = req.body;
+      const result = await productsCollection.insertOne(singleProduct);
+      res.send(result);
+    });
+    // In below code we are trying to delete an existing product from the server
+    app.delete("/products/:id", async (req, res) => {
+      const query = req.body;
+      console.log(query);
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // deliver stock (updating quantity)
+    app.put("/products/:id", async(req, res) => {
+      const id = req.params;
+      const filter = { _id: ObjectId(id) };
+      const productQnt = req.body.quantity;
+      const newQnt = productQnt - 1;
+      const options = { upsert: true };
+      const updateProduct = {
+        $set: {
+          quantity: newQnt
+        },
+      };
+      const updatedQnt = await productsCollection.updateOne(filter, updateProduct, options);
+      console.log(req.body.quantity);
+      res.send(updatedQnt);
+    });
   } finally {
     // await client.close();
   }
@@ -40,5 +70,5 @@ run().catch(console.dir);
 // MongoDB connection ends
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Assignment 11 app listening on port ${port}`);
 });
